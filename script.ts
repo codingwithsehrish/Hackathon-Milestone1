8e7ca4380cb1c40efe675cfe59bb280b578efb13
Sehrish M.Saleem Khatri <163769583+codingwithsehrish@users.noreply.github.com>
2024-11-15 02:05:17 +0500

    This is First Static Resume Please Click & Check
    This is My simple accessible Visible Own Build Static Resume

---
+++ script.ts
@@ -0,0 +1,16 @@
+import Script from "next/script";
+
+const toggleButton = document.getElementById("Toggle-skills") as HTMLButtonElement
+const skills = document.getElementById("skills") as HTMLButtonElement
+
+toggleButton.addEventListener("click",()=>{
+    if (skills.style.display === "none" ) {
+        skills.style.display = "block" 
+    } else {
+    skills.style.display = "none"
+    }
+    
+
+"build";"webpage "}
+
+);
